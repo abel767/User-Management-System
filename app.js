9fbae71ee3c13ee2257d1de4885407274a5a12c7
abel767 <abelthomas946@gmail.com>
2024-10-12 16:12:31 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,15 @@
+// this will be our server app js
+
+
+// connecting express 
+const express = require('express')
+const app = express();
+
+
+
+
+
+// server port 
+app.listen(2000, ()=>{
+    console.log("http://localhost:2000")
+})
